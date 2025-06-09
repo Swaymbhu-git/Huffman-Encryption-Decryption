cfed281de36fcd18285f4d5496769dcf794a3208
Himanshu Patel <142696007+Swaymbhu-git@users.noreply.github.com>
2025-06-10 00:39:31 +0530

    Final changes
    I wanted to deploy on vercel so did some changes.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Huffman-Encryption-Decryption/', // Replace 'your-repo-name' with the repository name
-  plugins: [react()],
+  base: './',
+  plugins: [react()]
 });
